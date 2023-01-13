e216a7d11a6e453bb47f9300626c3e115bd978b9
Wayne Schuller <wayne@schuller.id.au>
2023-01-14 10:37:40 +1100

    Rename processedData for clarity of purpose

--- src/utils/processData.js
+++ src/utils/processData.js
@@ -29,24 +29,24 @@ export function processData(
   let isRefresh = false;
 
   // Do we already have a set of visualizerE1RMLineData? Then this must be a refresh caused by changing the equation method
-  let processedData = [];
+  let visualizerE1RMLineData = [];
   if (visualizerData && visualizerData.visualizerE1RMLineData) {
     console.log(`... refreshing old data...`);
-    processedData = visualizerData.visualizerE1RMLineData; // We are going to discretely mutate React state to modify chart without a rerender
+    visualizerE1RMLineData = visualizerData.visualizerE1RMLineData; // We are going to discretely mutate React state to modify chart without a rerender
     isRefresh = true;
   }
 
   let equation = localStorage.getItem("equation");
   if (!equation) equation = "Brzycki"; // Probably not needed. Just in case.
 
-  processEstimateData(parsedData, processedData, equation);
+  processEstimateData(parsedData, visualizerE1RMLineData, equation);
 
   console.log(
-    `   ...processVisualiserData() complete: ${processedData.length} different types of lifts. (${equation} equation)`
+    `   ...processVisualiserData() complete: ${visualizerE1RMLineData.length} different types of lifts. (${equation} equation)`
   );
 
   // Remove any left over stale items (needed for refreshing data from Google Sheets)
-  processedData.forEach((liftType) => {
+  visualizerE1RMLineData.forEach((liftType) => {
     // Loop backwards through e1rmLineData mutating it to remove stale entries
     for (let i = liftType.data.length - 1; i >= 0; i--) {
       if (liftType.data[i].isUpdated === false) {
@@ -60,10 +60,10 @@ export function processData(
   // Let's sort each data array by date (x entry) so it draws lines correctly
   // (FIXME: write a custom YYYY-MM-DD compare function as 'new Date' in a sort function is frowned upon)
   // FIXME: if we presort parsedData, then .data will already be sorted?
-  processedData.forEach((arr) => arr.data.sort((a, b) => new Date(a.x) - new Date(b.x)));
+  visualizerE1RMLineData.forEach((arr) => arr.data.sort((a, b) => new Date(a.x) - new Date(b.x)));
 
   // Also sort our processedData so the most popular lift types get charts first
-  processedData.sort((a, b) => b.data.length - a.data.length);
+  visualizerE1RMLineData.sort((a, b) => b.data.length - a.data.length);
 
   // If we have many lift types (> 15) then intelligently ignore uninteresting data.
   // We remove any lift types if:
@@ -72,57 +72,57 @@ export function processData(
   // 2) we only did that lift type less than 10 times.
   //
   // Advice to user: Keep lifting or do lots of a lift type to keep it visualized
-  if (processedData.length > 15) {
+  if (visualizerE1RMLineData.length > 15) {
     const twoYearsInMilliseconds = 2 * 365 * 24 * 60 * 60 * 1000;
     const currentTime = Date.now();
 
-    for (let i = processedData.length - 1; i >= 0; i--) {
-      const lastDate = new Date(processedData[i].data[processedData[i].data.length - 1].x);
+    for (let i = visualizerE1RMLineData.length - 1; i >= 0; i--) {
+      const lastDate = new Date(visualizerE1RMLineData[i].data[visualizerE1RMLineData[i].data.length - 1].x);
 
-      if (currentTime - lastDate.getTime() > twoYearsInMilliseconds && processedData[i].data.length < 10) {
+      if (currentTime - lastDate.getTime() > twoYearsInMilliseconds && visualizerE1RMLineData[i].data.length < 10) {
         // processedData[i].data.splice(0); // delete the minor obsolete lift chart data (but we keep the PRs)
-        processedData.splice(i, 1); // delete the minor obsolete lift
+        visualizerE1RMLineData.splice(i, 1); // delete the minor obsolete lift
       }
     }
   }
 
   // Let's only keep the top 10 remaining lifts.
-  processedData.splice(10); // FIXME: this could be configurable in settings
+  visualizerE1RMLineData.splice(10); // FIXME: this could be configurable in settings
 
   // Get the top lifts by session for the main Analyzer Pie chart
-  let analyzerPieData = processAnalyzerPieData(parsedData, processedData);
-  let analyzerPRCardData = processAnalyzerPRCardData(parsedData, processedData);
+  let analyzerPieData = processAnalyzerPieData(parsedData, visualizerE1RMLineData);
+  let analyzerPRCardData = processAnalyzerPRCardData(parsedData, visualizerE1RMLineData);
 
   // Do we have a localStorage selectedLifts item? First time user will not have one.
   const selectedLiftsItem = localStorage.getItem("selectedLifts");
   if (selectedLiftsItem === null) {
-    let selectedLifts = processedData.map((item) => item.label);
+    let selectedLifts = visualizerE1RMLineData.map((item) => item.label);
     localStorage.setItem("selectedLifts", JSON.stringify(selectedLifts)); // Default to select all
   }
 
   // Process the PRs/Achivements and return some chartjs annotation config.
   if (isRefresh) {
     // If we have annotations this function will just mutate them
-    updateAchievements(processedData, equation, visualizerData.achievementAnnotations);
+    updateAchievements(visualizerE1RMLineData, equation, visualizerData.achievementAnnotations);
   } else {
     // If annotations are null this function will give us a fresh set
-    var annotations = processAchievements(parsedData, processedData, equation);
+    var annotations = processAchievements(parsedData, visualizerE1RMLineData, equation);
   }
 
   // 10 day padding for the beginning and end of our data makes chart look nice
   // Use the most popular lift to set some aesthetic x-axis padding at start and end
   // There is a chance loading another data set will require a new range, but unlikely.
   // FIXME: just check ALL the first tuples in every lift and use the most recent one.
-  let padDateMin = new Date(processedData[0].data[0].x); // First tuple in first lift
+  let padDateMin = new Date(visualizerE1RMLineData[0].data[0].x); // First tuple in first lift
   padDateMin = padDateMin.setDate(padDateMin.getDate() - 10);
 
   // FIXME: this could just do 10 days from now
-  let padDateMax = new Date(processedData[0].data[processedData[0].data.length - 1].x); // Last tuple in first lift
+  let padDateMax = new Date(visualizerE1RMLineData[0].data[visualizerE1RMLineData[0].data.length - 1].x); // Last tuple in first lift
   padDateMax = padDateMax.setDate(padDateMax.getDate() + 10);
 
   // Search through the processed data and find the largest y value
   let highestWeight = -1;
-  processedData.forEach((liftType) => {
+  visualizerE1RMLineData.forEach((liftType) => {
     liftType.data.forEach((lift) => {
       if (lift.y > highestWeight) highestWeight = lift.y;
     });
@@ -144,7 +144,7 @@ export function processData(
       padDateMax: padDateMax,
       highestWeight: highestWeight,
       achievementAnnotations: annotations,
-      visualizerE1RMLineData: processedData,
+      visualizerE1RMLineData: visualizerE1RMLineData,
       // We could wrap the datasets for chartjs here, but nevermind we will do it in the <Line /> component
       // visualizerE1RMLineData: {datasets: processedData},
     });
@@ -161,9 +161,9 @@ export function processData(
 //  Best lift is defined as the highest e1rm estimate.
 //  Because we might be mutating existing visualiserData we need to pass it in.
 // ------------------------------------------------------------------------------------------------
-function processEstimateData(parsedData, processedData, equation) {
+function processEstimateData(parsedData, visualizerE1RMLineData, equation) {
   for (const lift of parsedData) {
-    const liftIndex = getProcessedLiftIndex(processedData, lift.name);
+    const liftIndex = getProcessedLiftIndex(visualizerE1RMLineData, lift.name);
 
     // Main task - find the best e1rm estimate on this date
     let oneRepMax = estimateE1RM(lift.reps, lift.weight, equation);
@@ -177,11 +177,11 @@ function processEstimateData(parsedData, processedData, equation) {
     if (!url) url = "";
 
     // Do we already have any processed data on this date?
-    let dateIndex = processedData[liftIndex].data.findIndex((processedLift) => processedLift.x === lift.date);
+    let dateIndex = visualizerE1RMLineData[liftIndex].data.findIndex((processedLift) => processedLift.x === lift.date);
 
     if (dateIndex === -1) {
       // Push new lift tuple on this new date (in chartjs friendly format)
-      processedData[liftIndex].data.push({
+      visualizerE1RMLineData[liftIndex].data.push({
         x: lift.date,
         y: oneRepMax,
         label: label,
@@ -202,34 +202,34 @@ function processEstimateData(parsedData, processedData, equation) {
     // If we are changing equation method, then update the y value
     // FIXME: what happens if changing the equation changes the top estimate lift for that session?
     // FIXME: should we be setting .isUpdated here?
-    if (processedData[liftIndex].data[dateIndex].method !== equation) {
-      processedData[liftIndex].data[dateIndex].y = oneRepMax;
-      processedData[liftIndex].data[dateIndex].method = equation;
+    if (visualizerE1RMLineData[liftIndex].data[dateIndex].method !== equation) {
+      visualizerE1RMLineData[liftIndex].data[dateIndex].y = oneRepMax;
+      visualizerE1RMLineData[liftIndex].data[dateIndex].method = equation;
       continue; // Continue iterating through parsedData
     }
 
     // If this processed lift is stale and is the same e1rm/date as this parsed lift, then refresh it
     // This is important for refreshing data from Google Sheets
     if (
-      processedData[liftIndex].data[dateIndex].isUpdated === false &&
-      oneRepMax === processedData[liftIndex].data[dateIndex].y
+      visualizerE1RMLineData[liftIndex].data[dateIndex].isUpdated === false &&
+      oneRepMax === visualizerE1RMLineData[liftIndex].data[dateIndex].y
     ) {
-      processedData[liftIndex].data[dateIndex].isUpdated = true;
+      visualizerE1RMLineData[liftIndex].data[dateIndex].isUpdated = true;
       continue; // Continue iterating through parsedData
     }
 
     // If the parsed lift e1rm is higher than what we had on this date, then update.
     // Because our chart always has the best lift per day
-    if (oneRepMax > processedData[liftIndex].data[dateIndex].y) {
-      processedData[liftIndex].data[dateIndex].y = oneRepMax;
-      processedData[liftIndex].data[dateIndex].label = label;
-      processedData[liftIndex].data[dateIndex].notes = lift.notes;
-      processedData[liftIndex].data[dateIndex].method = equation;
-      processedData[liftIndex].data[dateIndex].isUpdated = true;
-      processedData[liftIndex].data[dateIndex].url = url;
-      processedData[liftIndex].data[dateIndex].unitType = lift.unitType;
-      processedData[liftIndex].data[dateIndex].reps = lift.reps;
-      processedData[liftIndex].data[dateIndex].weight = lift.weight;
+    if (oneRepMax > visualizerE1RMLineData[liftIndex].data[dateIndex].y) {
+      visualizerE1RMLineData[liftIndex].data[dateIndex].y = oneRepMax;
+      visualizerE1RMLineData[liftIndex].data[dateIndex].label = label;
+      visualizerE1RMLineData[liftIndex].data[dateIndex].notes = lift.notes;
+      visualizerE1RMLineData[liftIndex].data[dateIndex].method = equation;
+      visualizerE1RMLineData[liftIndex].data[dateIndex].isUpdated = true;
+      visualizerE1RMLineData[liftIndex].data[dateIndex].url = url;
+      visualizerE1RMLineData[liftIndex].data[dateIndex].unitType = lift.unitType;
+      visualizerE1RMLineData[liftIndex].data[dateIndex].reps = lift.reps;
+      visualizerE1RMLineData[liftIndex].data[dateIndex].weight = lift.weight;
       continue;
     }
   }

8f859fe09f852d33c6705b167987e41de1af6376
Wayne Schuller <wayne@schuller.id.au>
2023-11-23 09:09:39 +1100

    Sample chart

--- src/pages/visualizer.js
+++ src/pages/visualizer.js
@@ -8,6 +8,29 @@ const inter = Inter({ subsets: ["latin"] });
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
 const Visualizer = () => {
   const [time, setTime] = useState(1);
 
@@ -25,8 +48,34 @@ const Visualizer = () => {
         <h1 className="flex-1 scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl ">
           Strength Visualizer
         </h1>
+        <div className="mt-6">
+          <Chart />
+        </div>
       </div>
     </>
   );
 };
 export default Visualizer;
+
+const Chart = () => {
+  return (
+    <Line
+      datasetIdKey="id"
+      data={{
+        labels: ["Jun", "Jul", "Aug"],
+        datasets: [
+          {
+            id: 1,
+            label: "",
+            data: [5, 6, 7],
+          },
+          {
+            id: 2,
+            label: "",
+            data: [3, 2, 1],
+          },
+        ],
+      }}
+    />
+  );
+};

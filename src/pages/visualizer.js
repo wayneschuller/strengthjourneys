be2fba5f78dbfc8394828ebbd3a54d5d6abead2b
Wayne Schuller <wayne@schuller.id.au>
2023-11-24 09:30:51 +1100

    Set tick/grid colors from shadcn css variables

--- src/pages/visualizer.js
+++ src/pages/visualizer.js
@@ -2,6 +2,7 @@
 
 import Head from "next/head";
 import React, { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
@@ -67,6 +68,30 @@ const Visualizer = () => {
 export default Visualizer;
 
 const Chart2 = ({}) => {
+  const { theme } = useTheme();
+  const [primaryColor, setPrimaryColor] = useState("");
+  const [mutedColor, setMutedColor] = useState("");
+  const [mutedForegroundColor, setMutedForegroundColor] = useState("");
+
+  // const tickColor = theme === "dark" ? "white" : "black";
+  const lineColor = theme === "dark" ? "#222" : "#BBB";
+
+  useEffect(() => {
+    console.log("getting css colors...");
+    // Accessing the HSL color variable
+    const root = document.documentElement;
+
+    const computedMutedColor =
+      getComputedStyle(root).getPropertyValue("--muted");
+    const computedMutedForegroundColor =
+      getComputedStyle(root).getPropertyValue("--muted-foreground");
+
+    setMutedColor(convertToHslFormat(computedMutedColor));
+    setMutedForegroundColor(convertToHslFormat(computedMutedForegroundColor));
+    // console.log(computedMutedForegroundColor);
+    // console.log(convertToHslFormat(computedMutedForegroundColor));
+  }, [theme]);
+
   // Function to calculate 1RM using Brzycki formula
   function calculateOneRepMax(weight, reps) {
     return weight * (1 - (0.025 * reps) / 100);
@@ -97,7 +122,7 @@ const Chart2 = ({}) => {
     arr.sort((a, b) => new Date(a[0]) - new Date(b[0]));
   });
 
-  console.log(liftArrays);
+  // console.log(liftArrays);
 
   // Convert liftArrays to Chart.js compatible format
   const chartData = Object.entries(liftArrays).map(([lift, data]) => ({
@@ -105,36 +130,35 @@ const Chart2 = ({}) => {
     data: data.map(([date, value]) => ({ x: date, y: value })),
   }));
 
-  console.log(chartData);
+  // console.log(chartData);
 
-  return (
-    <Line
-      options={{ responsive: true }}
-      data={{
-        options: {
-          scales: {
-            x: {
-              type: "time", // Use 'time' for a time-based x-axis
-              time: {
-                unit: "day", // Display units by day
-              },
-              title: {
-                display: true,
-                text: "Date",
-              },
-            },
-            y: {
-              title: {
-                display: true,
-                text: "Weight (lb)",
-              },
-            },
-          },
+  const options = {
+    scales: {
+      x: {
+        ticks: {
+          color: mutedForegroundColor,
         },
-        datasets: chartData,
-      }}
-    />
-  );
+        grid: {
+          color: mutedForegroundColor,
+        },
+      },
+      y: {
+        ticks: {
+          color: mutedForegroundColor,
+        },
+        grid: {
+          color: mutedForegroundColor,
+        },
+      },
+    },
+    plugins: {
+      title: {
+        display: false,
+      },
+    },
+  };
+
+  return <Line options={options} data={{ datasets: chartData }} />;
 };
 
 // Function to generate random colors
@@ -145,4 +169,19 @@ function getRandomColor() {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
+}
+
+function convertToHslFormat(originalHsl) {
+  // Split the original HSL string into individual components
+  const [hue, saturation, lightness] = originalHsl.split(" ");
+
+  // Extract numeric values
+  const numericHue = parseFloat(hue);
+  const numericSaturation = parseFloat(saturation);
+  const numericLightness = parseFloat(lightness);
+
+  // Construct the HSL format string
+  const hslFormat = `hsl(${numericHue}, ${numericSaturation}%, ${numericLightness}%)`;
+
+  return hslFormat;
 }

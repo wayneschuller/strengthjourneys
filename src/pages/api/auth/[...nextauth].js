dede42388e7a8aeb7e2d0a6c487913c6ad5550b3
Wayne Schuller <wayne@schuller.id.au>
2023-11-28 07:55:46 +1100

    callback with token returned

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,6 +2,14 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
+const scopes = [
+  "https://www.googleapis.com/auth/userinfo.email",
+  "https://www.googleapis.com/auth/userinfo.profile",
+  "https://www.googleapis.com/auth/drive.file",
+];
+
+const JWT_SECRET = String(process.env.NEXTAUTH_SECRET);
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -10,12 +18,33 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
-          scope:
-            "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive.file",
+          scope: scopes.join(" "),
         },
       },
     }),
   ],
+  secret: JWT_SECRET,
+  jwt: {
+    encryption: true,
+    secret: JWT_SECRET,
+  },
+  debug: process.env.NODE_ENV === "development",
+  callbacks: {
+    jwt: async (token, user, account) => {
+      if (account) {
+        token.accessToken = account?.accessToken;
+        token.refreshToken = account?.refreshToken;
+      }
+
+      return token;
+    },
+    session: async (session, user) => {
+      session.accessToken = user.accessToken;
+      session.refreshToken = user.refreshToken;
+
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);

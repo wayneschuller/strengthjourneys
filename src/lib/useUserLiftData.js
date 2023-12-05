4f66bd07f5e96eb9a7292b12b8bb0ea397f5667e
Wayne Schuller <wayne@schuller.id.au>
2023-12-06 00:20:47 +1100

    useSWRImmutable for now

--- src/lib/useUserLiftData.js
+++ src/lib/useUserLiftData.js
@@ -1,11 +1,12 @@
 import useSWR from "swr";
+import useSWRImmutable from "swr/immutable";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json()); // Generic fetch for useSWR
 
 function useUserLiftData(session, ssid) {
   let shouldFetch = session && ssid ? true : false; // Only fetch if we have session and ssid
 
-  const { data, isLoading } = useSWR(
+  const { data, isLoading } = useSWRImmutable(
     shouldFetch ? `/api/readGSheet?ssid=${ssid}` : null,
     fetcher,
     {

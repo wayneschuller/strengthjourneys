cc362586d81be2663b1ef94668024755e07d55e8
Wayne Schuller <wayne@schuller.id.au>
2024-03-22 09:45:44 +1100

    Make GET method explict for clarity

--- src/pages/api/readGSheet.js
+++ src/pages/api/readGSheet.js
@@ -33,6 +33,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(url, {
+      method: "GET",
       headers: {
         Authorization: `Bearer ${session.accessToken}`,
       },

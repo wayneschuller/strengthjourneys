89076a64c12db3a288258d6d37af7d96e0eec0fa
Wayne Schuller <wayne@schuller.id.au>
2024-05-30 17:12:48 +1000

    x

--- src/components/visualizer/visualizer-chart.js
+++ src/components/visualizer/visualizer-chart.js
@@ -149,7 +149,7 @@ export default function VisualizerChart() {
   let defaultRangeInMonths = 6; // Desktop default
   let xPaddingInDays = 15; // Desktop default
   if (isMobile) {
-    defaultRangeInMonths = 1; // Mobile default
+    defaultRangeInMonths = 2; // Mobile default
     xPaddingInDays = 3; // Mobile default
   }
 

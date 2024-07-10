145491e6f33ab0cc3d0c127df939d3586ad76f9c
Wayne Schuller <wayne@schuller.id.au>
2024-07-10 21:07:10 +1000

    x

--- src/components/analyzer/circular-progress-with-letter.js
+++ src/components/analyzer/circular-progress-with-letter.js
@@ -90,6 +90,7 @@ const thresholds = [
 ];
 
 // Function to determine grade and HSL color based on progress
+// FIXME: 202407 with the shadcn charts the hue color is not working?
 const getGradeAndColor = (progress) => {
   for (let i = 0; i < thresholds.length; i++) {
     if (progress >= thresholds[i].minProgress) {

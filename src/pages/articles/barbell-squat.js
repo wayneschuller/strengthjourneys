f87045444164574ec1667825e4bbdee57ce30e78
Wayne Schuller <wayne@schuller.id.au>
2024-08-07 19:42:00 +1000

    Refactor article layout and component structure

--- src/pages/articles/barbell-squat.js
+++ src/pages/articles/barbell-squat.js
@@ -21,54 +21,81 @@ const StrengthJourneys = () => (
 
 export default function Article() {
   return (
-    <div className="mx-4 mb-10 flex items-center justify-center">
+    // <div className="mx-4 mb-10 flex items-center justify-center">
+    <div className="mx-4 mb-4 md:mx-[5vw]">
       <Head>
         <title>{title}</title>
         <meta name="description" content={title} />
       </Head>
-      <Card className="shadow-lg shadow-primary-foreground ring-0 ring-black hover:ring-1 dark:ring-white">
-        <CardHeader></CardHeader>
-        <CardContent>
-          <ArticleContent />
-        </CardContent>
-      </Card>
+
+      <h1 className="mb-8 flex-1 scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl">
+        {title}
+      </h1>
+      <h3 className="prose prose-orange flex justify-center dark:prose-invert">
+        Resources collated by <StrengthJourneys /> Staff
+      </h3>
+
+      <div className="mt-4 grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
+        <QuotesCard />
+        <VideoCard />
+      </div>
     </div>
   );
 }
 
-function ArticleContent() {
+function QuotesCard() {
   return (
-    <article className="prose prose-orange dark:prose-invert">
-      <header>
-        <h1>{title}</h1>
-        <h3>
-          Resources collated by <StrengthJourneys /> Staff
-        </h3>
-      </header>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/C_VtOYc6j5c"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/jyopTyOjXb0"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/nhoikoUEI8U"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
-    </article>
+    <Card>
+      <CardHeader>
+        <CardTitle>Quotes on the Back Squat</CardTitle>
+      </CardHeader>
+      <CardContent>
+        “There is simply no other exercise, and certainly no machine, that
+        produces the level of central nervous system activity, improved balance
+        and coordination, skeletal loading and bone density enhancement,
+        muscular stimulation and growth, connective tissue stress and strength,
+        psychological demand and toughness, and overall systemic conditioning
+        than the correctly performed full squat.” ― Mark Rippetoe, Starting
+        Strength
+      </CardContent>
+    </Card>
+  );
+}
+
+function VideoCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Squat Overview Video Guides</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-4">
+          <iframe
+            // width="560"
+            height="315"
+            src="https://www.youtube.com/embed/C_VtOYc6j5c"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+          <iframe
+            // width="560"
+            height="315"
+            src="https://www.youtube.com/embed/jyopTyOjXb0"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+          <iframe
+            // width="560"
+            height="315"
+            src="https://www.youtube.com/embed/nhoikoUEI8U"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </CardContent>
+    </Card>
   );
 }

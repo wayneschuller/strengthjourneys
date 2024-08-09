a69b7960b50db847c0e0f2e86d0f84ae64e862f8
Wayne Schuller <wayne@schuller.id.au>
2024-08-09 10:33:09 +1000

    Add state management for age, gender, and weight

--- src/pages/strength-level-calculator.js
+++ src/pages/strength-level-calculator.js
@@ -2,9 +2,6 @@
 
 import { useState, useEffect } from "react";
 import Head from "next/head";
-import { estimateE1RM } from "@/lib/estimate-e1rm";
-import { useRouter } from "next/router";
-import { Button } from "@/components/ui/button";
 import { UnitChooser } from "@/components/unit-type-chooser";
 import {
   Card,
@@ -37,6 +34,31 @@ import {
 } from "@/components/ui/select";
 
 export default function StrengthLevelCalculator() {
+  const [age, setAge] = useState(30);
+  const [gender, setGender] = useState("male");
+  const [weight, setWeight] = useState(200);
+  const [isMetric, setIsMetric] = useState(false);
+
+  const toggleIsMetric = (isMetric) => {
+    let newWeight;
+
+    if (!isMetric) {
+      // Going from kg to lb
+      newWeight = Math.round(weight * 2.2046);
+      setIsMetric(false);
+    } else {
+      // Going from lb to kg
+      newWeight = Math.round(weight / 2.2046);
+      setIsMetric(true);
+    }
+
+    setWeight(newWeight);
+
+    // Save in localStorage for this browser device
+    localStorage.setItem("calcIsMetric", JSON.stringify(isMetric));
+    // localStorage.setItem("weight", JSON.stringify(newWeight));
+  };
+
   return (
     <div className="mx-4 flex flex-row items-center md:mx-[5vw]">
       <Head>
@@ -61,14 +83,20 @@ export default function StrengthLevelCalculator() {
               <Input
                 id="age"
                 type="number"
-                defaultValue={30}
                 placeholder="Enter your age"
                 className="w-20"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
               />
             </div>
             <div className="flex items-center space-x-2">
               <Label htmlFor="gender">Gender</Label>
-              <Select id="gender" defaultValue="male" className="min-w-52">
+              <Select
+                id="gender"
+                value={gender}
+                onValueChange={(value) => setGender(value)}
+                className="min-w-52"
+              >
                 <SelectTrigger>
                   <SelectValue placeholder="Select gender" />
                 </SelectTrigger>
@@ -86,8 +114,13 @@ export default function StrengthLevelCalculator() {
                 id="weight"
                 type="number"
                 placeholder="Enter your weight"
-                defaultValue={200}
+                value={weight}
+                onChange={(e) => setWeight(e.target.value)}
                 className="w-24"
+              />
+              <UnitChooser
+                isMetric={isMetric}
+                onSwitchChange={toggleIsMetric}
               />
             </div>
           </div>

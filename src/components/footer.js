9a5dc94e1a1679e0acac3131509b3c0c7cae5947
Wayne Schuller <wayne@schuller.id.au>
2024-08-19 07:03:21 +1000

    Implement footer component with attribution links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,51 @@
 /** @format */
 
 "use client";
+import { featurePages } from "@/pages";
 
 export function Footer() {
-  // return <div>Thanks for using.</div>;
-  return <div></div>;
+  // return null;
+
+  return (
+    <footer className="py-6 md:px-8 md:py-0">
+      <div className="container flex flex-col items-center gap-2 text-muted-foreground md:h-24 md:flex-row md:gap-4">
+        <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+          Built by{" "}
+          <a
+            href="https://x.com/wayneschuller"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            Wayne Schuller
+          </a>
+          .
+        </p>
+        <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+          The source code is available on{" "}
+          <a
+            href="https://github.com/wayneschuller/strengthjourneys"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+        <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+          Buy me a{" "}
+          <a
+            href="https://buymeacoffee.com/lrhvbjxzqr"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            coffee
+          </a>
+          .
+        </p>
+      </div>
+    </footer>
+  );
 }

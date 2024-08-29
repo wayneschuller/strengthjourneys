c659ad63703f71bbe1016aad9d06748643264dde
Wayne Schuller <wayne@schuller.id.au>
2024-08-30 07:54:33 +1000

    Add playlist fetching utility function

---
+++ src/lib/playlist-utils.js
@@ -0,0 +1,46 @@
+import { kv } from "@vercel/kv";
+import { devLog } from "@/lib/processing-utils";
+
+// Fetch playlists and merge in separate vote data along the way.
+// Abstracted to use in both api route and also in ISR getStaticProps build step
+export async function fetchPlaylists(id = null) {
+  try {
+    if (id) {
+      // Fetch a specific playlist
+      const playlist = await kv.hget("playlists", id);
+      if (!playlist) {
+        throw new Error("Playlist not found");
+      }
+      return JSON.parse(playlist);
+    } else {
+      // Fetch all playlists
+      const playlists = await kv.hgetall("playlists");
+
+      // Map through all the playlists and add in the upvotes/downvotes
+      const playlistsWithVotes = await Promise.all(
+        Object.entries(playlists).map(async ([id, playlist]) => {
+          const votes = await kv.hmget(
+            `playlists:${id}`,
+            "upVotes",
+            "downVotes",
+          );
+          devLog(votes);
+          // If kv.hmget returns null, initialize counts to 0
+          const upVotes = parseInt(votes?.upVotes) || 0;
+          const downVotes = parseInt(votes?.downVotes) || 0;
+
+          return {
+            ...JSON.parse(playlist),
+            upVotes: upVotes,
+            downVotes: downVotes,
+          };
+        }),
+      );
+
+      return playlistsWithVotes;
+    }
+  } catch (error) {
+    console.error("Error fetching playlist(s):", error);
+    throw new Error("Error fetching playlist(s)");
+  }
+}

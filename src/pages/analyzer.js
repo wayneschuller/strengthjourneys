c3b00dc086c6b3095d0da254f96083e305f51639
Wayne Schuller <wayne@schuller.id.au>
2024-09-02 21:00:29 +1000

    Add SEO metadata and OG image to Analyzer page

--- src/pages/analyzer.js
+++ src/pages/analyzer.js
@@ -17,6 +17,7 @@ import { LiftTypeFrequencyPieCard } from "@/components/analyzer/lift-frequency-p
 import { MonthsHighlightsCard } from "@/components/analyzer/months-highlights-card";
 import { ActivityHeatmapsCard } from "@/components/analyzer/heatmap-card";
 import { InspirationCard } from "@/components/analyzer/inspiration-card";
+import { TitleSEOHead } from "@/components/title-seo";
 
 export default function Analyzer() {
   const { data: session, status: authStatus } = useSession();
@@ -30,40 +31,51 @@ export default function Analyzer() {
       </div>
     );
 
+  // OG Meta Tags
+  const URL = "https://www.strengthjourneys.xyz/strength-level-calculator";
+  const description =
+    "Unlock insights into your strength training progress with our PR Analyzer. Track consistency, view monthly highlights, and visualize your lifting journey. Get personalized analysis and inspiration to fuel your strength gains.";
+  const title = "PR Analyzer - Strength Progress Reports | Strength Journeys";
+  const ogImage =
+    "https://www.strengthjourneys.xyz/strength_journeys_strength_levels_analyzer_og.png";
+
   return (
-    <div className="mx-4 mb-4 md:mx-[5vw]">
-      <Head>
-        <title>PR Analyzer (Strength Journeys)</title>
-        <meta name="description" content="Strength Journeys Lift PR Analyzer" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <TitleSEOHead
+        title={title}
+        description={description}
+        canonicalURL={URL}
+        ogImage={ogImage}
+      />
 
-      <h1 className="mb-8 flex-1 scroll-m-20 text-center text-4xl font-extrabold tracking-tight md:hidden lg:text-5xl">
-        PR Analyzer
-      </h1>
+      <main className="mx-4 mb-4 md:mx-[5vw]">
+        <h1 className="mb-8 flex-1 scroll-m-20 text-center text-4xl font-extrabold tracking-tight md:hidden lg:text-5xl">
+          PR Analyzer
+        </h1>
 
-      <div className="mt-4 grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
-        <div className="flex h-full flex-col">
-          <SessionAnalysisCard />
-        </div>
-        <div className="flex h-full flex-col gap-6">
-          <ConsistencyCard />
-          <MonthsHighlightsCard />
-        </div>
-        <div className="grid h-full gap-6">
-          <div className="">
-            <InspirationCard />
-          </div>
+        <div className="mt-4 grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
           <div className="flex h-full flex-col">
-            <LiftTypeFrequencyPieCard />
+            <SessionAnalysisCard />
           </div>
+          <div className="flex h-full flex-col gap-6">
+            <ConsistencyCard />
+            <MonthsHighlightsCard />
+          </div>
+          <div className="grid h-full gap-6">
+            <div className="">
+              <InspirationCard />
+            </div>
+            <div className="flex h-full flex-col">
+              <LiftTypeFrequencyPieCard />
+            </div>
+          </div>
+          <div className="col-span-full">
+            <ActivityHeatmapsCard />
+          </div>
+          <Separator className="col-span-full" />
         </div>
-        <div className="col-span-full">
-          <ActivityHeatmapsCard />
-        </div>
-        <Separator className="col-span-full" />
-      </div>
-      <SelectedLiftsIndividualLiftCards />
-    </div>
+        <SelectedLiftsIndividualLiftCards />
+      </main>
+    </>
   );
 }

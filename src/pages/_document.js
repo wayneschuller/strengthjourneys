d7a17255106efde00d32d1680155e3f8286f90e4
Wayne Schuller <wayne@schuller.id.au>
2024-09-02 21:09:43 +1000

    Remove unnecessary "use client" directive from document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,16 +1,14 @@
 /** @format */
 
-"use client";
-
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-     <Head>
-        <script 
-          defer 
-          data-domain="strengthjourneys.xyz" 
+      <Head>
+        <script
+          defer
+          data-domain="strengthjourneys.xyz"
           src="https://plausible.io/js/script.js"
         />
         {/* Other head elements */}

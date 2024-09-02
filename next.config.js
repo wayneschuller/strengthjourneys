7484a1d90f44b8f91bbc031816352dce692e30e1
Wayne Schuller <wayne@schuller.id.au>
2024-09-02 15:11:36 +1000

    Add temporary redirects for legacy articles

--- next.config.js
+++ next.config.js
@@ -13,6 +13,24 @@ const nextConfig = {
       },
     ],
   },
+
+  // These two articles were manually jsxed before we had sanity.io 202409
+  // We could delete these in a few months once Google indexing has caught up
+  async redirects() {
+    return [
+      {
+        source: "/articles/own-your-lifting-data",
+        destination:
+          "/articles/the-power-of-owning-your-lifting-data-with-google-sheets",
+        permanent: true,
+      },
+      {
+        source: "/articles/henry-rollins-the-iron-and-the-soul",
+        destination: "/articles/the-iron-and-the-soul-author-henry-rollins",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

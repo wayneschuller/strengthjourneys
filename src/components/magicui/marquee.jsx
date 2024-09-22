a4599d8d1f60d941881455800c3526949398512c
Wayne Schuller <wayne@schuller.id.au>
2024-09-23 07:09:49 +1000

    Add Marquee component and animation to Tailwind config

---
+++ src/components/magicui/marquee.jsx
@@ -0,0 +1,39 @@
+import { cn } from "@/lib/utils";
+
+export default function Marquee({
+  className,
+  reverse,
+  pauseOnHover = false,
+  children,
+  vertical = false,
+  repeat = 4,
+  ...props
+}) {
+  return (
+    (<div
+      {...props}
+      className={cn(
+        "group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] [gap:var(--gap)]",
+        {
+          "flex-row": !vertical,
+          "flex-col": vertical,
+        },
+        className
+      )}>
+      {Array(repeat)
+        .fill(0)
+        .map((_, i) => (
+          <div
+            key={i}
+            className={cn("flex shrink-0 justify-around [gap:var(--gap)]", {
+              "animate-marquee flex-row": !vertical,
+              "animate-marquee-vertical flex-col": vertical,
+              "group-hover:[animation-play-state:paused]": pauseOnHover,
+              "[animation-direction:reverse]": reverse,
+            })}>
+            {children}
+          </div>
+        ))}
+    </div>)
+  );
+}

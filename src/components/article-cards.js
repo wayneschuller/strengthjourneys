e584ce281fa38b82d9bf2c822be49509a540182f
Wayne Schuller <wayne@schuller.id.au>
2024-09-22 16:50:24 +1000

    Add center alignment to article card grid layout

--- src/components/article-cards.js
+++ src/components/article-cards.js
@@ -61,7 +61,7 @@ export function RelatedArticles({ articles }) {
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-cols-1 items-center justify-center gap-4 md:grid-cols-3 xl:grid-cols-4">
           {articles.map((article) => (
             <div key={article.slug} className="group rounded-lg border">
               <Link

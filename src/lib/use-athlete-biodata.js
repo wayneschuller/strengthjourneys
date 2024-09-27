6a527f91c5c24ab9ac2c8d13ec3831feab8e63c6
Wayne Schuller <wayne@schuller.id.au>
2024-09-27 20:55:42 +1000

    Add custom hook for athlete biodata management

---
+++ src/lib/use-athlete-biodata.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { useLocalStorage } from "usehooks-ts";
+import {
+  interpolateStandardKG,
+  LiftingStandardsKG,
+} from "@/lib/lifting-standards-kg";
+
+// A custom hook to get and store the athlete provided bio data in localstorage
+// Also provide some custom strength levels for the main lifts based on this bio data.
+export const useAthleteBioData = () => {
+  const [age, setAge] = useLocalStorage("AthleteAge", 30, {
+    initializeWithValue: false,
+  });
+  const [isMetric, setIsMetric] = useLocalStorage("calcIsMetric", false, {
+    initializeWithValue: false,
+  });
+  const [sex, setSex] = useLocalStorage("AthleteSex", "male", {
+    initializeWithValue: false,
+  });
+  const [bodyWeight, setBodyWeight] = useLocalStorage(
+    "AtheleteBodyWeight",
+    200,
+    {
+      initializeWithValue: false,
+    },
+  );
+  const [standards, setStandards] = useState({});
+
+  useEffect(() => {
+    const bodyWeightKG = isMetric
+      ? bodyWeight
+      : Math.round(bodyWeight / 2.2046);
+
+    const uniqueLiftNames = Array.from(
+      new Set(LiftingStandardsKG.map((item) => item.liftType)),
+    );
+    const newStandards = {};
+
+    uniqueLiftNames.forEach((liftType) => {
+      const standard = interpolateStandardKG(
+        age,
+        bodyWeightKG,
+        sex,
+        liftType,
+        LiftingStandardsKG,
+      );
+
+      if (isMetric) {
+        newStandards[liftType] = standard || {};
+      } else {
+        newStandards[liftType] = {
+          physicallyActive: Math.round(standard?.physicallyActive * 2.2046),
+          beginner: Math.round(standard?.beginner * 2.2046),
+          intermediate: Math.round(standard?.intermediate * 2.2046),
+          advanced: Math.round(standard?.advanced * 2.2046),
+          elite: Math.round(standard?.elite * 2.2046),
+        };
+      }
+    });
+
+    setStandards(newStandards);
+  }, [age, sex, bodyWeight, isMetric]);
+
+  return {
+    age,
+    setAge,
+    isMetric,
+    setIsMetric,
+    sex,
+    setSex,
+    bodyWeight,
+    setBodyWeight,
+    standards,
+  };
+};

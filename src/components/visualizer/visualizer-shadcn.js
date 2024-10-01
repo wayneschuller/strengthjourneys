a3aba0a85f8ff928526b1e223009453410b55932
Wayne Schuller <wayne@schuller.id.au>
2024-10-02 08:26:48 +1000

    Update tickFormatter to use first item's unitType

--- src/components/visualizer/visualizer-shadcn.js
+++ src/components/visualizer/visualizer-shadcn.js
@@ -229,8 +229,8 @@ export function VisualizerShadcn({ setHighlightDate }) {
               ]}
               hide={width < 1280}
               axisLine={false}
-              tickFormatter={(value, index) =>
-                `${value}${chartData[index].unitType}`
+              tickFormatter={
+                (value) => `${value}${chartData[0]?.unitType || ""}` // Default to first item's unitType
               }
               ticks={Array.from(
                 { length: Math.ceil(roundedMaxWeightValue / tickJump) },

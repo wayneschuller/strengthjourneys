8416937a16deab456d0294ef6b8222218987df2b
Wayne Schuller <wayne@schuller.id.au>
2024-10-07 21:53:10 +1100

    Replace anchor tag with Link component in nav-bar

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -352,7 +352,7 @@ function BigFourBarbellInsightsMenu() {
       return (
         <li>
           <NavigationMenuLink asChild>
-            <a
+            <Link
               ref={ref}
               className={cn(
                 "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -367,7 +367,7 @@ function BigFourBarbellInsightsMenu() {
               <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
                 {children}
               </p>
-            </a>
+            </Link>
           </NavigationMenuLink>
         </li>
       );

8b529c709090e8db4f1bdb94479f2eb8fbaf74b1
Wayne Schuller <wayne@schuller.id.au>
2025-04-11 18:56:29 +1000

    Remove commented-out MultiLiftTooltipContent function

--- src/components/visualizer/visualizer-shadcn.js
+++ src/components/visualizer/visualizer-shadcn.js
@@ -136,69 +136,6 @@ export function VisualizerShadcn({ setHighlightDate }) {
     return date.toLocaleString("en-US", { month: "short", day: "numeric" });
   };
 
-  // const MultiLiftTooltipContent = ({
-  //   active,
-  //   payload,
-  //   label,
-  //   selectedLiftTypes,
-  // }) => {
-  //   // devLog(payload);
-  //   if (active && payload && payload.length) {
-  //     // Right now we have put key info into the chartData paylod. But we could simply lookup the date in parsedData/topLifts for info
-  //     const tuple = payload[0].payload;
-
-  //     // devLog(tuple);
-  //     const dateLabel = getReadableDateString(tuple.date);
-  //     const tooltipsPerLift = [];
-
-  //     selectedLiftTypes.forEach((liftType) => {
-  //       const reps = tuple[`${liftType}_reps`];
-  //       const weight = tuple[`${liftType}_weight`];
-  //       const oneRepMax = tuple[`${liftType}`];
-  //       const unitType = tuple.unitType;
-
-  //       if (reps && weight && oneRepMax) {
-  //         let labelContent = "";
-  //         if (reps === 1) {
-  //           labelContent = `Lifted ${reps}@${weight}${unitType}`;
-  //         } else {
-  //           labelContent = `Potential 1@${oneRepMax}${unitType} from lifting ${reps}@${weight}${unitType}`;
-  //         }
-
-  //         const color = getLiftColor(liftType);
-  //         tooltipsPerLift.push({
-  //           liftType: liftType,
-  //           label: labelContent,
-  //           color: color,
-  //           reps: reps,
-  //         });
-  //       }
-  //     });
-
-  //     // devLog(liftLabels);
-
-  //     return (
-  //       <div className="grid min-w-[8rem] max-w-[24rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 py-1.5 text-xs shadow-xl">
-  //         <p className="font-bold">{dateLabel}</p>
-  //         {tooltipsPerLift.map(({ liftType, label, color, reps, index }) => (
-  //           <div key={liftType}>
-  //             <div className="flex flex-row items-center">
-  //               <div
-  //                 className="mr-1 h-2.5 w-2.5 shrink-0 rounded-[2px]"
-  //                 style={{ backgroundColor: color }} // Use css style because tailwind is picky
-  //               />
-  //               <div className="font-semibold">{liftType}</div>
-  //             </div>
-  //             <div className="">{label}</div>
-  //           </div>
-  //         ))}
-  //       </div>
-  //     );
-  //   }
-
-  //   return null;
-  // };
-
   return (
     <Card>
       <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">

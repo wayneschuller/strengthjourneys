594cf6660bd2d9feea204c5dedb6104e78046a83
Wayne Schuller <wayne@schuller.id.au>
2025-04-19 07:48:54 +1000

    Update Radix UI radio-group version to 1.3.2

--- src/components/ui/radio-group.jsx
+++ src/components/ui/radio-group.jsx
@@ -11,7 +11,7 @@ RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 
 const RadioGroupItem = React.forwardRef(({ className, ...props }, ref) => {
   return (
-    (<RadioGroupPrimitive.Item
+    <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
         "aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
@@ -21,7 +21,7 @@ const RadioGroupItem = React.forwardRef(({ className, ...props }, ref) => {
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
         <Circle className="h-2.5 w-2.5 fill-current text-current" />
       </RadioGroupPrimitive.Indicator>
-    </RadioGroupPrimitive.Item>)
+    </RadioGroupPrimitive.Item>
   );
 })
 RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName

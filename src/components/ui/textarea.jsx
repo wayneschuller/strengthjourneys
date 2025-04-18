cddb441b424e4686e601810da9c0df0519222c4d
Wayne Schuller <wayne@schuller.id.au>
2025-04-19 07:57:59 +1000

    Refactor Textarea component structure and styles

--- src/components/ui/textarea.jsx
+++ src/components/ui/textarea.jsx
@@ -4,13 +4,13 @@ import { cn } from "@/lib/utils"
 
 const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   return (
-    (<textarea
+    <textarea
       className={cn(
-        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className
       )}
       ref={ref}
-      {...props} />)
+      {...props} />
   );
 })
 Textarea.displayName = "Textarea"

096fb7abf35f07c88191796413ce40e63b2f6823
Wayne Schuller <wayne@schuller.id.au>
2025-10-13 12:39:45 +1100

    Update hero section transition duration to 800ms

--- src/pages/index.js
+++ src/pages/index.js
@@ -218,16 +218,16 @@ export default function Home() {
       />
       <main className="mx-4 mb-4 md:mx-[5vw]">
         {/* Fixed height hero section that turns into our onboarding section and then home dashboard */}
-        <div className="duration-400 relative flex h-[400px] items-center justify-center transition-all md:h-[500px]">
+        <div className="duration-800 relative flex min-h-[800px] items-center justify-center transition-all md:h-[500px]">
           {showHeroSection ? (
             <div
-              className={`duration-400 absolute inset-0 h-full w-full transition-all ${isFadingHero ? "pointer-events-none -translate-y-6 scale-95 opacity-0" : "translate-y-0 scale-100 opacity-100"} `}
+              className={`duration-800 absolute inset-0 h-full w-full transition-all ${isFadingHero ? "pointer-events-none -translate-y-6 scale-95 opacity-0" : "translate-y-0 scale-100 opacity-100"} `}
             >
               <HeroSection />
             </div>
           ) : (
             <div
-              className={`absolute inset-0 h-full w-full transition-opacity duration-500 ${showHeroSection ? "opacity-0" : "opacity-100"} `}
+              className={`duration-800 absolute inset-0 h-full w-full transition-opacity ${showHeroSection ? "opacity-0" : "opacity-100"} `}
             >
               <HomeDashboard />
             </div>

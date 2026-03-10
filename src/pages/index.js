df906f74562c7c704d51d007c4557086efd6e15d
Wayne Schuller <wayne@schuller.id.au>
2026-03-10 13:01:45 +1100

    Ungate strength club calculator on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -108,7 +108,6 @@ export const featurePages = [
     description:
       "Track your progress toward the classic barbell milestones: 200 press, 300 bench, 400 squat, 500 deadlift.",
     IconComponent: Mountain,
-    devOnly: true,
   },
   {
     href: "/articles",
